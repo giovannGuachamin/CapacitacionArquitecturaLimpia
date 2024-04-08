dd69fb732a2a7d5a873e83c139c826806a76a4b1
giovannGuachamin <166409180+giovannGuachamin@users.noreply.github.com>
2024-04-08 09:16:26 -0500

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Infraestructure;
+using AplicationCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+//Create a service builder for a console application
+var services = new ServiceCollection();
+//Instance a configuration object
+var configuration = new ConfigurationBuilder().Build();
+
+//Register the dependencies
+//Nota: En un ambiente real esto debe ejecutarse solo en ambiente de desarrollo, en ambiente de producción se debe registrar en contexto de base de datos
+Dependencias.ConfigureServices(services, configuration);
+
+services.AddScoped<IProductoRepository, ProductoRepository>();
+
+//Create a service provider
+var serviceProvider = services.BuildServiceProvider();
+
+//Get the repository
+var productoRepository = serviceProvider.GetService<IProductoRepository>();
+
+//Get all the products
+var products = productoRepository.ObtenerTodosLosProductos();
+
+foreach(var product in products)
+{
+    Console.WriteLine($"Id: { product.Id}, Nombre: {product.Nombre} Precio: {product.Precio}, Descuento: {product.Descuento}");
+}
